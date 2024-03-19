7259e215e14c6dbf1ec9516f219e90e7073e498e
Gusttavo Castro <gusttavoleandro2023@gmail.com>
2024-03-19 16:12:02 -0300

    criar o projeto chat-ai

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { Chat } from "@/components/chat";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen bg-slate-50 items-center justify-center"> 
+      <Chat />
+    </div>
+  );
+}
